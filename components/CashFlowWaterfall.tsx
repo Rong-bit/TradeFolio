74a1cfd9753601426d2b5e83d3e6dc2b0544cb73
Rong-bit <hcj382000@gmail.com>
2026-04-13 16:36:43 +0800

    Update CashFlowWaterfall.tsx

--- components/CashFlowWaterfall.tsx
+++ components/CashFlowWaterfall.tsx
@@ -183,7 +183,7 @@ const CashFlowWaterfall: React.FC<Props> = ({ rows, hideHeader }) => {
                 return value;
               }}
             />
-            <Bar dataKey="segPLPosSwapped" name="segPLPosSwapped" stackId="wf" fill={WF_COLOR_PL_POS} radius={[0, 0, 0, 0]} />
+            <Bar dataKey="segPLPosSwapped" name="segPLPosSwapped" stackId="wf" fill={WF_COLOR_PL_POS} radius={[0, 0, 0, 0]} legendType="none" />
             <Bar dataKey="segIncomeSwapped" name="segIncomeSwapped" stackId="wf" fill={WF_COLOR_DIVIDEND} radius={[0, 0, 0, 0]} legendType="none" />
             <Bar dataKey="segIncomeDefault" name="segIncomeDefault" stackId="wf" fill={WF_COLOR_DIVIDEND} radius={[0, 0, 0, 0]} />
             <Bar
