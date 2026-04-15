ac9b4a2748f306e038d571e4df038334fbd37043
Rong-bit <hcj382000@gmail.com>
2026-04-15 16:57:14 +0800

    Update Dashboard.tsx

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect, useMemo, useRef, useCallback } from 'react';
 import { Account, CashFlow, CashFlowType, Currency, Holding, AssetClass } from '../portfolioTypes';
 import { formatCurrency, valueInBaseCurrency, getDisplayRateForBaseCurrency, holdingValueToTWD, buildAttributionSeries, buildWaterfallYearRows, buildQuarterlyTrendData, getAssetClassForTicker, calculateAssetAllocation } from '../utils/calculations';
