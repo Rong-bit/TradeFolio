e8f17c8875b58e5d7081793a34f8bda977a9fddf
Rong-bit <hcj382000@gmail.com>
2026-05-08 11:31:00 +0800

    Add yearWithheldNhiTwd field to portfolio type

--- portfolioTypes.ts
+++ portfolioTypes.ts
@@ -176,6 +176,7 @@ export interface PortfolioSummary {
   accumulatedCashDividendsTWD: number;
   accumulatedStockDividendsTWD: number;
   avgExchangeRate: number;
+  yearWithheldNhiTwd: number;
   yearUsWithholdingTwd: number;
 }
 
